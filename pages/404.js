9ec127ebd9b42b0414e0353afe31897641c06cb6
Everest Oreizy <43426819+everestoreizy@users.noreply.github.com>
2021-01-28 14:11:49 -0800

    initial dump of tutorial stuff

---
+++ pages/404.js
@@ -0,0 +1,3 @@
+export default function PNF404() {
+    return <div><title>PNF-404</title><h1>PNF-404</h1> <p>Looks like you've stumbled upon the planet of the Pikmin.</p></div>
+}
---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Date from '../components/date'
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  /*
+  const res = await fetch('..')
+  return res.json()
+  //if we wanted to use an external thing that returns json
+  */
+  
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hi, I'm Everest, a random high school student you don't care about.</p>
+        <p>
+          (This is a sample website built with next.js. Any post earlier than 2021 is a sample post and was NOT written by me.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li classname={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
